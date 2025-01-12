2ba061256b2177a61a5541f294bf8fefd54205da
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-01-12 14:16:04 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -189,7 +189,7 @@ show_menu() {
             clear
             echo -e "${YW}=== $MENU_TITLE ===${CL}"
             echo "1) $OPTION_1"
-            echo "1) $OPTION_2"
+            echo "2) $OPTION_2"
             echo "2) $OPTION_3"
             echo "q) $EXIT_MENU"
             echo
