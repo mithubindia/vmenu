f9207f92bb23091573f604ec927a378d437f4738
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-02-02 11:51:33 +0100

    Update main_menu.sh

--- scripts/menus/main_menu.sh
+++ scripts/menus/main_menu.sh
@@ -40,7 +40,7 @@ show_menu() {
     case $OPTION in
         1) exec bash <(curl -s "$REPO_URL/scripts/menus/hw_grafics_menu.sh") ;;
         2) exec bash <(curl -s "$REPO_URL/scripts/menus/storage_menu.sh") ;;
-        3) exec bash <(curl -s "$REPO_URL/scripts/menus/network_menu") ;;
+        3) exec bash <(curl -s "$REPO_URL/scripts/menus/network_menu.sh") ;;
         4) exec bash <(curl -s "$REPO_URL/scripts/menus/config_menu.sh") ;;
         5) clear; msg_ok "$(translate "Thank you for using ProxMenu. Goodbye!")"; exit 0 ;;
         *) msg_error "Opción inválida"; sleep 2 ;;
