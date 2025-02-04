1f4ce25d96fd4cf567c96f537e9d59fa6536bc0b
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-02-04 18:44:42 +0100

    Update install_coral_lxc.sh

--- scripts/install_coral_lxc.sh
+++ scripts/install_coral_lxc.sh
@@ -40,7 +40,7 @@ initialize_cache
 
 # Select LXC container
 select_container() {
-    clear
+
     CONTAINERS=$(pct list | awk 'NR>1 {print $1, $3}' | xargs -n2)
     if [ -z "$CONTAINERS" ]; then
         msg_error "$(translate 'No containers available in Proxmox.')"
