84263159cd048440bd093feac3a3a4e0e4d8cfed
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-02-13 16:46:10 +0100

    Create next.config.js

---
+++ web/next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "/ProxMenux/",
+  basePath: "/ProxMenux",
+  distDir: "out",
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      }
+    }
+    return config
+  },
+}
+
+module.exports = nextConfig
