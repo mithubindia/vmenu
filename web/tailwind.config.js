ffa490c7e38c3a599ab03a84d9fa9155ee988cc2
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-02-13 20:21:20 +0100

    Update tailwind.config.js

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["web/app/**/*.{ts,tsx}", "web/components/**/*.{ts,tsx}", "web/pages/**/*.{ts,tsx}"],
+ content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./pages/**/*.{ts,tsx}"],
   theme: {
     container: {
       center: true,
