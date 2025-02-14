a773b91f05827bad9ebc3a99c8d78d1f150dbfe1
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-02-14 19:21:23 +0100

    Update page.tsx

--- web/app/changelog/page.tsx
+++ web/app/changelog/page.tsx
@@ -5,7 +5,9 @@ async function getChangelog() {
   const changelogPath = path.join(process.cwd(), "..", "CHANGELOG.md")
   try {
     const fileContents = fs.readFileSync(changelogPath, "utf8")
-    return fileContents // Ahora el archivo ya es HTML, no necesita conversión
+
+   
+    return fileContents.trim()
   } catch (error) {
     console.error("Error reading changelog file:", error)
     return "<p>Changelog content not found.</p>"
@@ -21,7 +23,7 @@ export default async function ChangelogPage() {
         <h1 className="text-4xl font-bold mb-8">Changelog</h1>
         <div
           className="prose prose-lg max-w-none bg-gray-100 p-4 border border-gray-300 rounded-md"
-          dangerouslySetInnerHTML={{ __html: changelogContent }} // Renderiza directamente el HTML
+          dangerouslySetInnerHTML={{ __html: changelogContent }} 
         />
       </div>
     </div>
