e89da522e4c3946886f9b17da1763f779c263b9b
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-02-14 19:44:52 +0100

    Update tailwind.config.js

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -69,8 +69,39 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.foreground"),
+            a: {
+              color: theme("colors.primary.DEFAULT"),
+              "&:hover": {
+                color: theme("colors.primary.DEFAULT"),
+              },
+            },
+            h1: {
+              color: theme("colors.foreground"),
+            },
+            h2: {
+              color: theme("colors.foreground"),
+            },
+            h3: {
+              color: theme("colors.foreground"),
+            },
+            strong: {
+              color: theme("colors.foreground"),
+            },
+            code: {
+              color: theme("colors.foreground"),
+            },
+            figcaption: {
+              color: theme("colors.muted.foreground"),
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 }
 
