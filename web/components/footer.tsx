4a746126b1e443a77309e237289c2b432bd6e859
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-02-16 00:22:11 +0100

    Update footer.tsx

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
               className="hover:opacity-90 transition-opacity flex items-center"
             >
               <Image
-                src="/images/kofi.png"
+                src="https://raw.githubusercontent.com/MacRimi/ProxMenux/main/images/kofi.png"
                 alt="Support me on Ko-fi"
                 width={175}
                 height={50}
