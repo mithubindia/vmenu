081958dfb1db9a6e2a65395fc2b290b19429cfe0
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-02-16 12:53:24 +0100

    Update page.tsx

--- web/app/guides/page.tsx
+++ web/app/guides/page.tsx
@@ -3,6 +3,7 @@ import fs from "fs"
 import path from "path"
 import matter from "gray-matter"
 import Link from "next/link"
+import Footer from "@/components/footer"
 
 const guidesDirectory = path.join(process.cwd(), "..", "guides")
 
@@ -63,6 +64,7 @@ export default function GuidesPage() {
           ))}
         </div>
       </div>
+      <Footer />
     </div>
   )
 }
