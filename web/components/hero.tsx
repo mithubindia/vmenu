26ed80102d47b30fceb3446ad927b77bf40794a9
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-02-16 15:32:08 +0100

    Update hero.tsx

--- web/components/hero.tsx
+++ web/components/hero.tsx
@@ -14,9 +14,8 @@ export default function Hero() {
         </span>
       </h1>
       <p className="text-base sm:text-lg md:text-xl mb-8 max-w-4xl mx-auto text-gray-300">
-        ProxMenu is a tool designed to execute shell scripts in an organized manner, using a menu system with categories
-        to facilitate access and execution of various scripts hosted on GitHub. ProxMenu simplifies script usage, aiming
-        to improve productivity and streamline automated tasks.
+       ProxMenu is a tool for executing shell scripts in a user-friendly way, featuring a categorized menu system for easy access to scripts hosted on GitHub. 
+       Designed to facilitate productivity, it simplifies automation and streamlines task execution.
       </p>
       <div className="flex justify-center">
         <Button size="lg" className="bg-blue-500 hover:bg-blue-600" asChild>
