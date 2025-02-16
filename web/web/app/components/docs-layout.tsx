719fb36d82e89c008e0a09442808b223b2f9b6ab
MacRimi <ricoextincion@gmail.com>
2025-02-16 15:38:56 +0100

    update

--- web/web/app/components/docs-layout.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import type React from "react"
-
-export default function DocsLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-screen bg-white text-gray-900">{children}</div>
-}
-
