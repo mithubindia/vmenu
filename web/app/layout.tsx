6f54caef38fa8d5542cb3695b865c4c77062f164
MacRimi <ricoextincion@gmail.com>
2025-02-18 22:30:20 +0100

    Update

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -13,18 +13,34 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="dark">
       <head>
-        <title>{typeof metadata.title === "string" ? metadata.title : metadata.title?.default}</title>
+        <title>{metadata.title.default}</title>
         <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.openGraph?.title} />
-        <meta property="og:description" content={metadata.openGraph?.description} />
-        <meta property="og:image" content={metadata.openGraph?.images?.[0]?.url} />
-        <meta property="og:url" content={metadata.openGraph?.url} />
-        <meta property="og:type" content={metadata.openGraph?.type} />
-        <meta property="og:site_name" content={metadata.openGraph?.siteName} />
-        <meta name="twitter:card" content={metadata.twitter?.card} />
-        <meta name="twitter:title" content={metadata.twitter?.title} />
-        <meta name="twitter:description" content={metadata.twitter?.description} />
-        <meta name="twitter:image" content={metadata.twitter?.images?.[0]} />
+        <meta name="application-name" content={metadata.applicationName} />
+        <meta name="author" content={metadata.authors?.[0]?.name} />
+        <meta name="generator" content={metadata.generator} />
+        <meta name="keywords" content={metadata.keywords?.join(",")} />
+        <meta name="referrer" content={metadata.referrer} />
+        <meta name="creator" content={metadata.creator} />
+        <meta name="publisher" content={metadata.publisher} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:locale" content={metadata.openGraph.locale} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta property="og:image:width" content={metadata.openGraph.images[0].width?.toString()} />
+        <meta property="og:image:height" content={metadata.openGraph.images[0].height?.toString()} />
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta name="twitter:description" content={metadata.twitter.description} />
+        <meta name="twitter:image" content={metadata.twitter.images?.[0]} />
+        {metadata.icons.icon.map((icon, index) => (
+          <link key={index} rel="icon" type={icon.type} sizes={icon.sizes} href={icon.url} />
+        ))}
+        {metadata.icons.apple.map((icon, index) => (
+          <link key={index} rel="apple-touch-icon" sizes={icon.sizes} href={icon.url} />
+        ))}
       </head>
       <body className={`${inter.className} bg-background text-foreground antialiased`}>
         <Navbar />
