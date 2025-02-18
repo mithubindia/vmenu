3180fbe29ae9064fdaffb38229bd1dd09d58cbcf
MacRimi <ricoextincion@gmail.com>
2025-02-18 11:53:41 +0100

    Update

--- web/app/docs/layout.tsx
+++ web/app/docs/layout.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react"
-import DocSidebar from "@/components/doc-sidebar"
+import DocSidebar from "@/components/DocSidebar"
 
 interface LayoutProps {
   children: ReactNode
