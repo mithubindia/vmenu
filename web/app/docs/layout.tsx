cbb6fca3c7b27d2fd93cc9b60400c62547240e45
MacRimi <ricoextincion@gmail.com>
2025-02-18 13:04:41 +0100

    update

--- web/app/docs/layout.tsx
+++ web/app/docs/layout.tsx
@@ -5,12 +5,13 @@ import Footer from "@/components/footer"
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen bg-white text-gray-900">
-      <div className="flex flex-col md:flex-row flex-1 pt-16 md:pt-0">
+      <div className="flex flex-col lg:flex-row flex-1 pt-16 lg:pt-0">
         <DocSidebar />
-        <main className="flex-1 p-4 md:p-6">{children}</main>
+        <main className="flex-1 p-4 lg:p-6 pt-32 lg:pt-6 overflow-y-auto">
+          <div className="max-w-3xl mx-auto">{children}</div>
+        </main>
       </div>
-      <footer />
+      <Footer />
     </div>
   )
 }
-
