d1cbdef38c4c4543b05c3ad808935e3f86a32717
MacRimi <ricoextincion@gmail.com>
2025-02-18 20:20:35 +0100

    Update

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -7,18 +7,6 @@ const Resources = dynamic(() => import("@/components/resources"), { ssr: false }
 const SupportProject = dynamic(() => import("@/components/support-project"), { ssr: false })
 const Footer = dynamic(() => import("@/components/footer"), { ssr: false })
 
-export const metadata: Metadata = {
-  ...siteMetadata,
-  title: "ProxMenux - Home",
-  openGraph: {
-    ...siteMetadata.openGraph,
-    title: "ProxMenux - Home",
-  },
-  twitter: {
-    ...siteMetadata.twitter,
-    title: "ProxMenux - Home",
-  },
-}
 
 export default function Home() {
   return (
