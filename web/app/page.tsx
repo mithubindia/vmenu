5e521086e041f65a7c3aff7ef77fca6b82268957
MacRimi <ricoextincion@gmail.com>
2025-02-18 22:03:33 +0100

    Update

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic"
 import type { Metadata } from "next"
-import { metadata as siteMetadata } from './metadata'
+import { metadata as siteMetadata } from "./metadata"
 
 const Hero = dynamic(() => import("@/components/hero"), { ssr: false })
 const Resources = dynamic(() => import("@/components/resources"), { ssr: false })
@@ -10,24 +10,29 @@ const Footer = dynamic(() => import("@/components/footer"), { ssr: false })
 export const metadata: Metadata = {
   ...siteMetadata,
   title: "ProxMenux ",
-  description: "A menu-driven script for Proxmox VE management, designed to simplify and streamline the execution of commands and tasks.",
+  description:
+    "A menu-driven script for Proxmox VE management, designed to simplify and streamline the execution of commands and tasks.",
   openGraph: {
     ...siteMetadata.openGraph,
     title: "ProxMenux ",
-    description: "A menu-driven script for Proxmox VE management, designed to simplify and streamline the execution of commands and tasks.",
-    images: [{
-      url: 'https://macrimi.github.io/ProxMenux/main.png',
-      width: 1363,
-      height: 735,
-      alt: 'ProxMenux '
-    }]
+    description:
+      "A menu-driven script for Proxmox VE management, designed to simplify and streamline the execution of commands and tasks.",
+    images: [
+      {
+        url: "https://macrimi.github.io/ProxMenux/main.png",
+        width: 1363,
+        height: 735,
+        alt: "ProxMenux ",
+      },
+    ],
   },
   twitter: {
     ...siteMetadata.twitter,
     title: "ProxMenux",
-    description: "A menu-driven script for Proxmox VE management, designed to simplify and streamline the execution of commands and tasks.",
-    images: [`https://macrimi.github.io/ProxMenux/main.png`]
-  }
+    description:
+      "A menu-driven script for Proxmox VE management, designed to simplify and streamline the execution of commands and tasks.",
+    images: [`https://macrimi.github.io/ProxMenux/main.png`],
+  },
 }
 
 export default function Home() {
@@ -40,3 +45,4 @@ export default function Home() {
     </div>
   )
 }
+
