7f0a5908d72d62b601ee6b2a4b296b74ff981c7d
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-02-19 00:27:22 +0100

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css"
 import { Inter } from "next/font/google"
 import Navbar from "@/components/navbar"
 import MouseMoveEffect from "@/components/mouse-move-effect"
-import Footer from "@/components/footer"
 import type React from "react"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -84,7 +83,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Navbar />
         <MouseMoveEffect />
         <div className="pt-16 md:pt-16">{children}</div>
-        <Footer />
       </body>
     </html>
   )
