444002b006006c9f2db550b6711b469ca7b33c6f
MacRimi <123239993+MacRimi@users.noreply.github.com>
2025-06-10 12:44:49 +0200

    Update storage_menu.sh

--- scripts/menus/storage_menu.sh
+++ scripts/menus/storage_menu.sh
@@ -26,39 +26,32 @@ initialize_cache
 
 
 while true; do
-    OPTION=$(whiptail --title "$(translate "Disk and Storage Manager Menu")" --menu "$(translate "Select an option:")" 20 70 10 \
-        "1" "$(translate "Add Disk Passthrough to a VM")" \
-        "2" "$(translate "Add Disk") Passthrough $(translate "to a LXC")" \
-        "3" "$(translate "Import Disk Image to a VM")" \
-        "4" "$(translate "Return to Main Menu")" 3>&1 1>&2 2>&3)
+    OPTION=$(dialog --clear --backtitle "ProxMenux" --title "$(translate "Disk and Storage Manager Menu")" \
+                    --menu "\n$(translate "Select an option:")" 20 70 10 \
+                    "1" "$(translate "Add Disk Passthrough to a VM")" \
+                    "2" "$(translate "Add Disk") Passthrough $(translate "to a LXC")" \
+                    "3" "$(translate "Import Disk Image to a VM")" \
+                    "4" "$(translate "Return to Main Menu")" \
+                    2>&1 >/dev/tty) # Redirect dialog output to stderr, and then capture it
 
     case $OPTION in
         1)  
-		    #show_proxmenux_logo
-            #msg_info2 "$(translate "Running script: Add Disk Passthrough to a VM")..."
             clear
             bash <(curl -s "$REPO_URL/scripts/storage/disk-passthrough.sh")
             ;;
         2)
-		    #show_proxmenux_logo
-            #msg_info2 "$(translate "Running script: Add Disk Passthrough to a CT")..."
             clear
             bash <(curl -s "$REPO_URL/scripts/storage/disk-passthrough_ct.sh")
             ;;
         3)
-		    #show_proxmenux_logo
-            #msg_info2 "$(translate "Running script: Import Disk Image to a VM")..."
             clear
             bash <(curl -s "$REPO_URL/scripts/storage/import-disk-image.sh")
             ;;
         4)
-		    #show_proxmenux_logo
             exec bash <(curl -s "$REPO_URL/scripts/menus/main_menu.sh")
             ;;
         *)
-		    #show_proxmenux_logo
             exec bash <(curl -s "$REPO_URL/scripts/menus/main_menu.sh")
             ;;
     esac
 done
-
